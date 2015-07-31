4d4952b4c6ad6dd00c837b4b41b0312a9cbed31d
Alex4952 <keyzoo@gmail.com>
2015-07-31 23:11:26 +0900

    Initial Commit

---
+++ SnowAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SnowAnimation
+//
+//  Created by GIJU HONG on 7/31/15.
+//  Copyright (c) 2015 GIJU HONG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SnowAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SnowAnimation
+//
+//  Created by GIJU HONG on 7/31/15.
+//  Copyright (c) 2015 GIJU HONG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
